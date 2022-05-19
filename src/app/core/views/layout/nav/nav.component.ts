4b83e5a11251e812cc30d8a706e8dfb80bf01e9f
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2022-05-19 10:41:48 +0200

    Fix permissions

--- src/app/core/views/layout/nav/nav.component.ts
+++ src/app/core/views/layout/nav/nav.component.ts
@@ -35,8 +35,8 @@ export class NavComponent implements OnInit {
       {label: this.translate.instant('menu.schedule'), icon: PrimeIcons.CALENDAR, routerLink: '/welcome', visible: this.auth.hasRole([Role.Staff])},
       {label: this.translate.instant('menu.management.title'), icon: PrimeIcons.COG, visible: this.auth.hasRole([]),
         items:[
-          {label: this.translate.instant('menu.management.users'), icon: PrimeIcons.USERS, routerLink: '/welcome', visible: this.auth.hasRole([Role.Staff])},
-          {label: this.translate.instant('menu.management.estadistics'), icon: PrimeIcons.CHART_BAR, routerLink: '/welcome', visible: this.auth.hasRole([Role.Staff])}
+          {label: this.translate.instant('menu.management.users'), icon: PrimeIcons.USERS, routerLink: '/welcome', visible: this.auth.hasRole([])},
+          {label: this.translate.instant('menu.management.estadistics'), icon: PrimeIcons.CHART_BAR, routerLink: '/welcome', visible: this.auth.hasRole([])}
         ]
       },
       {label: this.translate.instant('menu.logOut'), icon: PrimeIcons.SIGN_OUT, command: () => { this.logout(); }}
